ae0b9ddf36e594de1a8472dfcd53fe88c97bc3e0
MeikeG6644 <144061381+MeikeG6644@users.noreply.github.com>
2023-11-07 21:17:23 +0100

    Delete scripts/oefenen.js

--- scripts/oefenen.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-console.log("hi");
-
-
-
-//Hamburger menu
-document.querySelector('.menu-button').addEventListener('click', function() {
-    document.querySelector('.menu-items').classList.toggle('active');
-});
